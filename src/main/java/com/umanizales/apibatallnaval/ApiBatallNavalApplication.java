054e3a8b80c2066f067571340e456835c5745397
crisa97 <cccruz76382@umanizales.edu.co>
2021-05-30 21:26:46 -0500

    Primera versión api batalla naval,datasource configurado

---
+++ src/main/java/com/umanizales/apibatallnaval/ApiBatallNavalApplication.java
@@ -0,0 +1,13 @@
+package com.umanizales.apibatallnaval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBatallNavalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBatallNavalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umanizales/apibatallnaval/ApiBatallNavalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umanizales.apibatallnaval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBatallNavalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
